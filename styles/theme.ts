51c294bae3d2565453e81eff746128ee2f528b35
kjof <andreeastroem@gmail.com>
2021-11-23 20:46:13 +0100

    chore: initialise project

---
+++ styles/theme.ts
@@ -0,0 +1,71 @@
+const theme = {
+  colors: {
+    primaryScale: [
+      "#8CF5D2",
+      "#79F3CB",
+      "#53F0BC",
+      "#2EEDAE",
+      "#13DF9B",
+      "#10B981",
+      "#0C855D",
+      "#075239",
+      "#031E15",
+      "#000000",
+    ],
+    secondaryScale: [
+      "#FFFFFF",
+      "#FFFFFF",
+      "#FBE9EF",
+      "#F5C6D4",
+      "#EFA3BA",
+      "#EA81A0",
+      "#E1517C",
+      "#D62459",
+      "#A61C45",
+      "#761431",
+    ],
+    tertiaryScale: [
+      "#D2D2DA",
+      "#C7C7D1",
+      "#B0B1BF",
+      "#9A9AAC",
+      "#84849A",
+      "#6E6F87",
+      "#555568",
+      "#3C3C49",
+      "#22232A",
+      "#09090B",
+    ],
+  },
+  radii: {
+    xs: "0.125rem",
+    sm: "0.25rem",
+    md: "0.375rem",
+    lg: "0.5rem",
+    xl: "0.75rem",
+  },
+  space: {
+    "3xs": "0.125rem",
+    "2xs": "0.25rem",
+    xs: "0.375rem",
+    sm: "0.5rem",
+    md: "0.75rem",
+    lg: "1rem",
+    xl: "1.25rem",
+    "2xl": "1.725rem",
+  },
+  fontSizes: {
+    sm: "0.75rem",
+    md: "1rem",
+    lg: "1.25rem",
+  },
+  fontWeights: {
+    normal: 500,
+    semibold: 600,
+    bold: 700,
+  },
+};
+
+export const themeType = typeof theme;
+
+export default theme;
